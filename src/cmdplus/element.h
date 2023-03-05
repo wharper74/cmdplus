1e605f8d733ae3953b3bda3079c1de3995a39979
Davide Gioia <davide_gioia@hotmail.com>
2023-03-05 18:31:58 +0100

    Some changes to the Element class

--- src/cmdplus/element.h
+++ src/cmdplus/element.h
@@ -10,30 +10,14 @@ namespace cmdplus {
       using id_type = int;
       using value_type = T;
 
-      Element(const Element::id_type& id, const std::string& name, const std::string& description, bool optional = true ) : _id(id), _name(name), _value(T()), _description(description), _optional(optional) {}
+      Element(const Element::id_type& id, const std::string& short_name, const std::string& full_name, const std::string& description, bool optional = true ) : _id(id), _short_name(short_name), _full_name(full_name), _value(T()), _description(description), _optional(optional) {}
       Element(const Element&) = delete;
-      Element(Element&& source) : _id(source._id), _name(source._name), _value(std::move(source._value)), _description(source._description), _optional(source._optional) {}
+      Element(Element&& source) = delete;
       ~Element() = default;
 
-      Element& operator=(const Element&) = delete;
-      Element& operator=(Element&& source) {
-        if( &source != this ) {
-          _id = std::move(source._id);
-          _name = std::move(source._name);
-          _value = std::move(source._value);
-          _description = std::move(source._description);
-          _optional = std::move(source._optional);
-        }
-      }
-
-      bool& operator==(const Element& other) {
-        return (this == &other || this->getName() == other.getName() );
-      }
-      bool& operator!=(const Element& other) {
-        return ! ((*this) == other);
-      }
-
-      const std::string& getName() const noexcept { return _name;}
+      const Element::id_type getId() const noexcept { return _id; }
+      const std::string& getShortName() const noexcept { return _short_name;}
+      const std::string& getFullName() const noexcept { return _full_name; }
       const Element::value_type& getValue() const noexcept {return _value;}
       const std::string& getDescription() const noexcept {return _description;}
 
@@ -42,9 +26,15 @@ namespace cmdplus {
 
     private:
       Element::id_type _id; /// element identifier
-      std::string _name; /// element name
-      Element::value_type _value; /// element value
-      std::string _description; /// full description of the element
       bool _optional; /// <b>true</b> if the element is optional
+
+      std::string _short_name; /// the element name when prefixed with a single "-"
+      std::string _full_name; // the element name when prefixed with a double "-"
+      
+      Element::value_type _value; /// element value
+      
+
+      std::string _description; /// full description of the element
+      
   };
 }
